7accce780c143387c6eaa50534b231b6c3a41fad
ppidcard <tvxqlikei@live.cn>
2021-08-05 15:24:53 +1000

    Done

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id 
+        @name = name 
+        @type = type 
+        @db = db
+    end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon
+        WHERE id = ?
+        SQL
+
+        res = db.execute(sql, id).first
+        new_poke = Pokemon.new(id: res[0], name: res[1], type: res[2], db:db)
+
+    end
 end
